790e61ad744562dfc98e2813262b6e498d31cfc4
Elena Morton <elena.uleia@gmail.com>
2017-05-04 18:44:48 +0000

    Update source file adding 'fizzbuzz'.

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,14 @@
 
-
-def fizzbuzz(number)
-    
+def fizzbuzz(num)
+    return num if num == 0
+    if num % 15 == 0
+        return "fizzbuzz"
+    end
+    if num % 3 == 0
+        return "fizz"
+    end
+    if num % 5 == 0
+        return "buzz"
+    end
+    return num
 end
