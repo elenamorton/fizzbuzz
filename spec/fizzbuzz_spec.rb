38e2651081965aa49282bbbfdab0bc085d272d5b
Elena Morton <elena.uleia@gmail.com>
2017-05-04 19:12:50 +0000

    Adding more TC in the spec file.

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,5 +1,6 @@
 require 'fizzbuzz'
 
+
 describe 'fizzbuzz' do
     it 'returns "fizz" when passed 0' do
         expect(fizzbuzz(0)).to eq 0
@@ -7,7 +8,7 @@ describe 'fizzbuzz' do
 end
 
 describe 'fizzbuzz' do
-    it 'returns "fizz" when passed 3' do
+    it 'returns "fizz" for the number 3' do
         expect(fizzbuzz(3)).to eq 'fizz'
     end
 end
@@ -21,5 +22,23 @@ end
 describe 'fizzbuzz' do
     it 'returns "buzz" when passed 15' do
         expect(fizzbuzz(15)).to eq 'fizzbuzz'   
+    end
+end
+
+describe 'fizzbuzz' do
+    it 'returns "buzz" when passed 30' do
+        expect(fizzbuzz(30)).to eq 'fizzbuzz'   
+    end
+end
+
+describe 'fizzbuzz' do
+    it 'returns "buzz" when passed 45' do
+        expect(fizzbuzz(45)).to eq 'fizzbuzz'   
+    end
+end
+
+describe 'fizzbuzz' do
+    it 'returns "buzz" when passed 75' do
+        expect(fizzbuzz(75)).to eq 'fizzbuzz'   
     end
 end
